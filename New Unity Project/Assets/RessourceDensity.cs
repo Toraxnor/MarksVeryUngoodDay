44c77536152e071ea81522438e1ad33f17e0e8e3
Elandiel <khorne@gmx.net>
2016-05-01 13:20:38 +0200

    h

---
+++ New Unity Project/Assets/RessourceDensity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RessourceDensity : MonoBehaviour {
+    public const int NumberOfRessources = 2;
+
+
+    public const double EarthPercent = 100;
+    public const int EarthAverageQuantity = 0;
+
+
+    public const double CopperPercent = 0.01;
+    public const int CopperAverageQuantity = 4;
+
+    /* Not implemented yet.
+    public const int StonePercent = 10;
+    public const int StoneAverageQuantity = 20;
+    */
+    
+}
